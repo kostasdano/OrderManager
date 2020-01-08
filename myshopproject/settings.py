0ae10d2dc65052143fb2354605805f6c6b86236d
Konstantinos Danochristos <konstantinos.danochristos@gmail.com>
2020-01-08 14:56:01 +0200

    heroku settings

--- myshopproject/settings.py
+++ myshopproject/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
