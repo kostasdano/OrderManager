0a742ec62e5cc3ec4eb0c74ad46b3347d41409d0
Konstantinos Danochristos <konstantinos.danochristos@gmail.com>
2020-01-09 12:06:59 +0200

    changed base.html

--- myshopproject/settings/base.py
+++ myshopproject/settings/base.py
@@ -1,21 +1,10 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import dj_database_url
-from dj_config_url import config
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4l5!6wefot62r^3z-@si7vvq5@im$3=kpz1^)joy4!w8o3$5iz'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,12 +50,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myshopproject.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -83,9 +66,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -95,9 +75,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
