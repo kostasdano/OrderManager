1cf01096d925add742b0beb20a1359aaad5ebc2b
Konstantinos Danochristos <konstantinos.danochristos@gmail.com>
2020-01-09 11:47:04 +0200

    changed settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshopproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshopproject.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
